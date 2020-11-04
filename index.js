8e07f77aae53f143e875e6e6e035ca896e65d0c0
Paola Santillan <pao.chxnge@gmail.com>
2020-11-04 08:02:02 -0300

    node+express routes

--- index.js
+++ index.js
@@ -1,94 +1,103 @@
-const http = require("http");
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 const NODE_ENV = process.env.NODE_ENV;
 
-let server = http.createServer((req, res) => {
-    console.log("I received a request to" + req.url);
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<b>Hello world</b>");
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+//const saludo = require('./saludo');
+
+let file;
+
+// fs https://www.tutorialesprogramacionya.com/javascriptya/nodejsya/detalleconcepto.php?punto=4&codigo=4&inicio=0 
+const loadFile = () => file = JSON.parse(fs.readFileSync(process.env.DB, 'utf-8'));
+const saveFile = () => file = fs.writeFileSync(process.env.DB, JSON.stringify(file));
+
+//console.log(saludo);
+
+//Middleware
+
+/**
+ * El cuerpo de una peticion contiene información desde una petición tipo POST cuando un cliente
+ * desea crear una nueva entidad/registro o actualizar uno existente mediante PUT.
+ * Para accesar a la información del cuerpo de dicha petición usamos el modulo npm body-parser,
+ * que permite realizar esta tarea. No es necesario programarla. Solo se require instalar body-parser 
+ * y habilitar json() asi como url-encode como middlewares para convertir datos a JSON.
+ */
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({}))
+
+//Mètodos HTTP https://developer.mozilla.org/es/docs/Web/HTTP/Methods 
+
+app.get('/', (req, res) =>{
+    loadFile();
+    res.send(file);
 });
 
-//////PRUEBAS
+//http://localhost:2000/query?id=nuestraID
 
-/*var miVariable = "texto";
-console.log(miVariable);
-let miVariableLet = "otroTexto";
-console.log(miVariableLet);
+app.get('/query', (req,res) => {
+    res.send(req.query.id)
+});
 
-function varTest(){
-    var a = 100;
-    if(true) {
-        var a = 99;
-        console.log(a);
+app.get('/:id', (req,res) => {
+    loadFile();
+    const result = file.find(obj => parseInt(obj._id) === parseInt(req.params.id));
+    res.send(result)
+});
+
+app.put('/:id', (req,res) => {
+    loadFile();
+    const result = file.find(obj => parseInt(obj._id) === parseInt(req.params.id));
+    //Object.assign copia todas las propiedades enumerables de uno o más objetos fuente a un objeto destino. 
+    //Devuelve el objeto destino.
+    //reemplazo lo que tengo (result) con lo que viene (req.body)
+    Object.assign(result,req.body);
+    saveFile();
+    res.sendStatus(200);
+});
+
+app.delete('/:id', (req,res,next) => {
+    loadFile();
+
+    /**
+     * con el == antes de hacer la comparación se convierten ambos datos a un tipo común.
+     * Con === ninguno de estos valores se convierte de manera implícita antes de ser comparado.
+     * Por eso aunque el valor del dato sea igual, si el tipo de dato no coincide el operador responde que no son iguales.
+     */
+
+    const result = file.find(obj => parseInt(obj._id) === parseInt(req.params.id));
+
+    //reemplazo lo que tengo (result) con el borrado lógico
+    Object.assign(result,{deleted:true, deletedAt: new Date()});
+    saveFile();
+    res.sendStatus(200);
+});
+
+app.post('/', (req,res,next) =>{
+    /**
+     * try...catch señala un bloque de instrucciones a intentar (try), 
+     * y especifica una respuesta si se produce una excepción (catch).
+    */
+    try{
+        loadFile();
+        const object = req.body;
+        const _id = file.length + 1;
+        file.push({
+            ... object,
+            _id
+        });
+        saveFile();
+        res.send({success: true});
+    }catch (err){
+        next(err);
     }
-    console.log(a);
-}
+});
 
-function letTest(){
-    let a = 100;
-    if(true) {
-        let a = 99;
-        console.log(a);
-    }
-    console.log(a);
-}
+app.listen(PORT, () => console.log(`Started at port ${PORT} in ${NODE_ENV} environment`));
 
-varTest();
-letTest();
-
-var unaVariableRepetidaVar = "es var";
-var unaVariableRepetidaVar = "es otra var";
-console.log(unaVariableRepetidaVar);
-
-let unaVariableRepetida = 10;
-//No puedo crear dos variables let porque el motor me devuelve un error
-//let unaVariableRepetida = 100;
-
-function hacerAlgo(){
-    console.log(variableUno);
-    console.log(variableDos);
-    var variableUno;
-    //let variableDos;
-}
-
-hacerAlgo();
-
-const unValorConstante = 100;
-console.log(unValorConstante);
-// No puedo reasignar el valor a una constante
-// unValorConstante = 20;
-function unaFuncion(){
-    const unValorConstante = 200;
-    console.log(unValorConstante);
-}
-unaFuncion();
-
-/*
- (parametro1,parametro2,...,parametroN) => 
- { 
-     sentenciaUno; 
-     sentenciaDos; 
-     return "resultado";
- }
-
- (parametro1,parametro2,...,parametroN) => expresion;
-
- parametroUnico => expresion;
-*/
-
-/*let otraFuncion = function() { return "Hola" };
-console.log(otraFuncion());
-
-let miSaludo = nombre => "Hola " + nombre;
-let resultado = miSaludo("Paola");
-console.log(resultado);*/
-
-/////PRUEBAS
-
-//server.listen(PORT);
-
-server.listen(PORT, () => {
+/*app.listen(PORT, () => {
     console.log(`Started at port ${PORT} in ${NODE_ENV} environment`);
-});
+});*/
---
+++ saludo.js
@@ -0,0 +1,1 @@
+module.exports = 'Saludo desde saludo.js';
